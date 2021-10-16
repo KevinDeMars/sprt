ce65886d3c98b92d2124fb702fc493542ecc8aa0
Kevin DeMars <kevin_demars1@baylor.edu>
2021-10-16 16:14:25 -0500

    Improved logging, added timeout + socket reuse
    Signed-off-by: Kevin DeMars <kevin_demars1@baylor.edu>

--- src/sprt/app/server/Server.java
+++ src/sprt/app/server/Server.java
@@ -12,19 +12,17 @@ import sprt.serialization.*;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class Server {
     private static final Logger LOG = Logger.getLogger(Server.class.getName());
     // Number of times socket.accept() is allowed to fail in a row before server gives up
     private static final int NUM_ACCEPT_TRIES = 10;
+    private static final int TIMEOUT_MS = 5 * 1000;
 
     protected enum Error {
         TooManyAcceptFails(0);
@@ -43,13 +41,7 @@ public class Server {
         int port = 12345;
         int numThreads = 10;
 
-        try {
-            var hnd = new FileHandler("connections.log");
-            //hnd.setFormatter(new SimpleFormatter());
-            LOG.addHandler(hnd);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        setupLogger();
 
         try {
             new Server(port, numThreads).go();
@@ -58,12 +50,38 @@ public class Server {
         }
     }
 
+    private static void setupLogger() {
+        try {
+            LOG.setLevel(Level.ALL);
+
+            var defaultHandlers = Logger.getLogger("").getHandlers();
+            for (var hnd : defaultHandlers) {
+                Logger.getLogger("").removeHandler(hnd);
+            }
+
+            var hnd = new FileHandler("connections.log");
+            hnd.setLevel(Level.ALL);
+            //System.setProperty("java.util.logging.SimpleFormatter.format", "")
+            hnd.setFormatter(new SimpleFormatter());
+            LOG.addHandler(hnd);
+
+            var hnd2 = new ConsoleHandler();
+            hnd2.setLevel(Level.ALL);
+            LOG.addHandler(hnd2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private ExecutorService threadPool;
     private ServerSocket socket;
 
     public Server(int port, int numThreads) throws IOException {
-        socket = new ServerSocket(port);
+        socket = new ServerSocket();
+        //LOG.info(socket.getLocalSocketAddress() + " " + socket.getLocalPort());
+        socket.setReuseAddress(true);
+        socket.bind(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), port));
         threadPool = Executors.newFixedThreadPool(numThreads);
     }
 
@@ -72,6 +90,8 @@ public class Server {
 
         int numAcceptFails = 0;
         while (true) {
+            // Repeatedly accept connections and handle in thread pool.
+            // If too many socket.accept() fails in a row, exit.
             try {
                 Socket clientSock = socket.accept();
                 numAcceptFails = 0;
@@ -90,15 +110,30 @@ public class Server {
     }
 
     private void handleClient(Socket cliSock) {
-        logClient(cliSock, "Client connected");
+        LOG.fine(logPrefix(cliSock) + "Client connected");
+
         try {
+            cliSock.setSoTimeout(TIMEOUT_MS);
             doHandleClient(cliSock);
         }
+        catch (SocketTimeoutException e) {
+            LOG.fine(logPrefix(cliSock) + "Client timed out");
+        }
         catch (IOException e) {
-            logClient(cliSock, Level.INFO, "I/O error", e);
+            LOG.log(Level.INFO, logPrefix(cliSock) + "I/O Error");
         }
         catch (Throwable t) {
-            logClient(cliSock, Level.SEVERE, "Uncaught exception handling client", t);
+            LOG.log(Level.SEVERE, logPrefix(cliSock) + "Uncaught exception handling client", t);
+        }
+        finally {
+            try {
+                cliSock.close();
+                LOG.fine(logPrefix(cliSock) + "Client connection closed");
+            }
+            catch (IOException e) {
+                LOG.log(Level.INFO, logPrefix(cliSock) + "Couldn't close client socket", e);
+            }
+
         }
     }
 
@@ -107,7 +142,6 @@ public class Server {
         var out = new MessageOutput(cliSock.getOutputStream());
 
         var req = (Request) Message.decodeType(in, MessageType.Request);
-        // For initial function, instantiate the class corresponding to that function's name
 
         var app = getApp(req.getFunction());
         if (app.isEmpty()) {
@@ -135,18 +169,21 @@ public class Server {
         Response response;
         do {
             var req = (Request) Message.decodeType(in, MessageType.Request);
+            LOG.finer(logPrefix(cliSock) + "Received: " + req);
+
             response = app.handleRequest(req);
             response.setCookieList(
                     req.getCookieList().addAll(response.getCookieList())
             );
             response.encode(out);
+            LOG.finer(logPrefix(cliSock) + "Sent: " + response);
         }
         while (!"NULL".equals(response.getFunction()));
 
     }
 
     private Optional<ServerApp> getApp(String name) {
-        Class<?> clazz = null;
+        Class<?> clazz;
         try {
             clazz = Class.forName("sprt.app.server.apps." + name + "." + name);
         } catch (ClassNotFoundException | NoClassDefFoundError e) {
@@ -154,7 +191,7 @@ public class Server {
             return Optional.empty();
         }
 
-        Object instance = null;
+        Object instance;
         try {
             instance = clazz.getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
@@ -170,14 +207,7 @@ public class Server {
     }
 
 
-    private void logClient(Socket cliSock, Level level, String msg, Throwable throwable) {
-        LOG.log(level, cliSock.getRemoteSocketAddress() + "-" + Thread.currentThread().getId() + " " + msg, throwable);
-    }
-    // Why doesn't Java have default parameters
-    private void logClient(Socket cliSock, Level level, String msg) {
-        logClient(cliSock, level, msg, null);
-    }
-    private void logClient(Socket cliSock, String msg) {
-        logClient(cliSock, Level.INFO, msg);
+    private static String logPrefix(Socket cliSock) {
+        return cliSock.getRemoteSocketAddress() + "-" + Thread.currentThread().getId() + " ";
     }
 }
